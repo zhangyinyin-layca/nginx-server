70b423da589dd7519b7205768d6eb0e59851482f
zhangyinyin <792124510@qq.com>
2024-12-03 14:08:06 +0800

    前端代码初始化

---
+++ frontend/web-code/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Vue 3 + Express.js Demo</h1>
+    <button @click="fetchData">获取数据</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ''
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('http://localhost:3000/api/data');
+        this.message = response.data.message;
+      } catch (error) {
+        console.error('There was an error!', error);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
